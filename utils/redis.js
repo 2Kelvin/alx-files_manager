f051f558941532fb29fe43a9162fc14ea61cd20e
2Kelvin <rocketmancodes02@gmail.com>
2024-03-07 13:01:23 +0300

    add redis client & utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,9 +1,41 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
 class RedisClient {
   constructor() {
-    // create a redis client here
+    // creating a redis client
+    this.client = createClient();
+    this.aliveClient = true;
+
+    this.client.on('error', (err) => {
+      this.aliveClient = false;
+      console.log(err);
+    });
+    this.client.on('connect', () => {
+      this.aliveClient = true;
+    });
   }
 
-  function isAlive() {
-    return;
+  // check if redis connected successfully
+  isAlive() {
+    return this.aliveClient;
+  }
+
+  // get the redis value for the passed in key
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  // set a redis key with an expiration time
+  async set(key, value, expirationTime) {
+    promisify(this.client.set).bind(this.client)(key, value, 'EX', expirationTime);
+  }
+
+  // deletes a redis key
+  async del(key) {
+    promisify(this.client.del).bind(this.client)(key);
   }
 }
+
+const redisClient = new RedisClient();
+export default redisClient;
