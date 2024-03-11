1c653895693509e4cfb29b5c773ab3b760bca3bf
2Kelvin <rocketmancodes02@gmail.com>
2024-03-11 10:43:01 +0300

    update router

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import { authBasic, authXToken } from '../middlewares/auth';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -12,5 +13,11 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', authBasic, AuthController.getConnect);
 router.get('/disconnect', authXToken, AuthController.getDisconnect);
 router.get('/users/me', authXToken, UsersController.getMe);
+router.post('/files', authXToken, FilesController.postUpload);
+router.get('/files/:id', authXToken, FilesController.getShow);
+router.get('/files', authXToken, FilesController.getIndex);
+router.put('/files/:id/publish', authXToken, FilesController.putPublish);
+router.put('/files/:id/unpublish', authXToken, FilesController.putUnPublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
